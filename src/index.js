ea1b0970f8ee30093e8e401bc8d86760dffe632c
nbee <nbeenbeing@gmail.com>
2024-12-13 16:49:04 -0500

    Adds drag and drop transform of svg file to hpgl file

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ console.log("RAW SVG!", svgString)
 
 // getPaths(parsedSvg)
 
-let path = pathFromSVG(svgString)
-const commands = svgToDrawbot(path, 200, { x: 0, y: 0 });
 
 // We know we have this structure, (an array of these bad bois)
 // command: "moveTo"
@@ -29,7 +27,7 @@ const commands = svgToDrawbot(path, 200, { x: 0, y: 0 });
 
 
 // The full set of supported commands is: PA, PR, PU, PD, CA, CP, CS, DI, DR, LB, SA, SI, SL, SM, SP, SR, SS, UC, LT, TL, VS, XT, YT, DF, DT, IM, IN, SC, DC, DP, OA, OC, OD, OE, OF, OH, OI, OO, OP, OS, OW, IP, IW.
-console.log("Commands!", commands)
+// console.log("Commands!", commands)
 
 const initialize = "IN;"
 const selectPen = (penNumber) => `SP${penNumber};`
@@ -55,9 +53,6 @@ const returnHPGLFromCommands = (commands) => {
   `
 }
 
-const result = returnHPGLFromCommands(commands)
-console.log("Result", result)
-
 // ui
 // const download = document.getElementById('download')
 // console.log("Download", download)
@@ -71,36 +66,98 @@ console.log("Result", result)
 const download = document.getElementById('drop_zone')
 console.log("Download", download)
 
-download.ondrop = dropHandler;
-
-const optimized = optimize(svgString, {
-  path: 'path-to.svg', // recommended
-  multipass: true, // all other config fields are available here
-});
-
-const optimizedSvgString = optimized.data;
-
-console.log("Optimized!", optimizedSvgString)
-
-const input_textarea = document.getElementById('input')
-input_textarea.innerText = svgString
 
 
-const output_textarea = document.getElementById('output')
-output_textarea.innerText = optimizedSvgString
+// const input_textarea = document.getElementById('input')
+// input_textarea.innerText = svgString
 
-function dropHandler(ev) {
-  console.log("File(s) dropped");
+
+// const output_textarea = document.getElementById('output')
+// output_textarea.innerText = optimizedSvgString
+
+function downloadBlob(blob, filename) {
+  // Create an object URL for the blob object
+  const url = URL.createObjectURL(blob);
+
+  // Create a new anchor element
+  const a = document.createElement('a');
+
+  // Set the href and download attributes for the anchor element
+  // You can optionally set other attributes like `title`, etc
+  // Especially, if the anchor element will be attached to the DOM
+  a.href = url;
+  a.download = filename || 'download';
+
+  // Click handler that releases the object URL after the element has been clicked
+  // This is required for one-off downloads of the blob content
+  const clickHandler = () => {
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+      removeEventListener('click', clickHandler);
+    }, 150);
+  };
+
+  // Add the click event listener on the anchor element
+  // Comment out this line if you don't want a one-off download of the blob content
+  a.addEventListener('click', clickHandler, false);
+
+  // Programmatically trigger a click on the anchor element
+  // Useful if you want the download to happen automatically
+  // Without attaching the anchor element to the DOM
+  // Comment out this line if you don't want an automatic download of the blob content
+  a.click();
+
+  // Return the anchor element
+  // Useful if you want a reference to the element
+  // in order to attach it to the DOM or use it in some other way
+  return a;
+}
+download.ondragover = (ev) => {
+  console.log("File(s) in drop zone");
 
   // Prevent default behavior (Prevent file from being opened)
   ev.preventDefault();
+}
 
+download.ondrop = (ev) => {
+  console.log("File(s) dropped");
+  
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+  
   if (ev.dataTransfer.items) {
     // Use DataTransferItemList interface to access the file(s)
     [...ev.dataTransfer.items].forEach((item, i) => {
       // If dropped items aren't files, reject them
       if (item.kind === "file") {
         const file = item.getAsFile();
+        const text = file.text().then( x => {
+          try{
+          // first optimize the svg to only include "paths" rather than predefined svg "shapes"
+          const optimized = optimize(x, {
+            path: 'path-to.svg', // recommended
+            multipass: true, // all other config fields are available here
+          });
+          
+          const optimizedSvgString = optimized.data;
+          // Now we parse the SVG to coordinates
+          let path = pathFromSVG(optimizedSvgString)
+          const commands = svgToDrawbot(path, 200, { x: 0, y: 0 });
+          // once we have the coordinates, we can translate to hpgl 
+          const result = returnHPGLFromCommands(commands)
+          console.log("Result", result)
+          const blob = new Blob([result], { type: "image/hpgl" });
+          console.log("Download blob now plzzzz")
+          downloadBlob(blob, "fuckinwerrrrrrrrque.hpgl")
+
+          // const url = URL.createObjectURL(blob);
+          // window.open(url, '_blank');
+
+          }catch(e){
+            console.log("ERROR : ", e)
+          }
+        })
+        console.log("File!", )
         console.log(`… file[${i}].name = ${file.name}`);
       }
     });
@@ -109,5 +166,15 @@ function dropHandler(ev) {
     [...ev.dataTransfer.files].forEach((file, i) => {
       console.log(`… file[${i}].name = ${file.name}`);
     });
+  }
+};
+
+
+download.onclick = () => {
+  if(optimizedSvgString){
+    const blob = new Blob([optimizedSvgString], { type: 'text/plain' });
+    downloadBlob(blob, 'test.hpgl');  
+  } else {
+    console.log("Nothing in the dragon drop bro")
   }
 }
